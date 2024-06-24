29b34e0100a7c2270da8b040eb04cab20338355f
variphx <variphx@gmail.com>
2024-06-24 12:27:03 +0700

    fjdks

---
+++ frontend/src/lib/components/ChuongTrinhHocCreateForm.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  type Nganh = {
+    id: string;
+    ten: string;
+  };
+
+  export let nganhs: Nganh[];
+</script>
+
+<div class="table-container">
+  <div class="table table-hover"></div>
+</div>
---
+++ frontend/src/routes/user/admin/chuong-trinh-hoc/lap-ho-so/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import ChuongTrinhHocCreateForm from "$lib/components/ChuongTrinhHocCreateForm.svelte";
+  import type { PageServerData } from "./$types";
+
+  export let data: PageServerData;
+  const nganhs = data.nganhs;
+</script>
+
+  <ChuongTrinhHocCreateForm {nganhs}></ChuongTrinhHocCreateForm>
